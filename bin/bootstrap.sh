8eb31836a1585c858def050fe661189a7eb73d46
kalail <kashif610@gmail.com>
2016-04-21 21:41:15 -0700

    update bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,41 +1,20 @@
 #!/bin/sh
 
-# install developer tools
-xcode-select --install
+# Update the system package definitions.
+apt-get update
 
-# install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
+# Install core tools.
+apt-get install -y \
+    aptitude \
+    build-essential \
+    hg \
+    htop \
+    tmux \
+    python \
+    python-pip \
+    httpie \
 
-# install binaries
-brew cask install firefoxdeveloperedition
-brew cask install iterm2
-brew cask install sublime-text3
-brew cask install google-chrome
-brew cask install dropbox
-brew cask install spotify
-brew cask install viscosity
-brew cask install dockertoolbox
-
-# install libraries
-brew install tmux
-brew install httpie
-brew install htop
-brew install postgresql
-brew install redis
-brew install geoip geoipupdate
-sudo easy_install pip
-sudo pip install cdiff
-sudo pip install virtualenv
-sudo pip install debug
-
-# link config files
-ln -s etc/.tmux.conf ~/.tmux.conf
-ln -s etc/.bash_profile ~/.bash_profile
-ln -s etc/.gitconfig ~/.gitconfig
-
-# create ssh keys
-ssh-keygen -t rsa -b 4096 -C "kashif610@gmail.com"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+# Copy config files.
+rm -f ~/.profile && cp etc/.profile ~/.profile
+rm -f ~/.tmux.conf && cp etc/.tmux.conf ~/.tmux.conf
+rm -f ~/.gitconfig && cp etc/.profile ~/.gitconfig
